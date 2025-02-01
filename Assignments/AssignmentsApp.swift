54e5ada1fc88fef6f287e3aabc1c1b4cd7e4b033
Sambhav Singh <ryugasingh90@gmail.com>
2025-02-01 14:15:43 +0530

    Initial Commit

---
+++ Assignments/AssignmentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentsApp.swift
+//  Assignments
+//
+//  Created by Sambhav Singh on 01/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
